9928a34d7c9d24a96fab1a2bb77d7fc43a3e1b09
happy2y2 <pjy1365@gmail.com>
2024-02-09 22:12:50 +0900

    scanf2

---
+++ scanf2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //문제2: num1xnum2+num3 만들기
+
+    int result1;
+    int num1, num2, num3;
+
+    printf("세개의 정수 입력:");
+    scanf("%d %d %d\n", &num1, &num2, &num3);
+
+    result1=num1*num2+num3;
+
+    printf("%d * %d + %d = %d \n", num1, num2, num3, result1);
+    return 0;
+
+}
